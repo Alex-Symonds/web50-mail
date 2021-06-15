6ff7acc3fe23b65ed975b776c58864b5880ca63b
Alex Symonds <alex-symonds@gmail.com>
2021-06-15 11:20:58 +0100

    Read email working

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -10,11 +10,14 @@ document.addEventListener('DOMContentLoaded', function() {
   load_mailbox('inbox');
  });
 
+
+
 function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#read-view').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -22,29 +25,60 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+
+
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#read-view').style.display = 'none';
 
   // Show the mailbox name
   const emv = document.querySelector('#emails-view');
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   // Load the table-looking div of emails
-  load_emails(mailbox, emv);
-
+  add_mailbox_container(mailbox, emv);
 }
 
 
-// Loading emails
-function load_emails(mailbox, emv){
-  fetch(`/emails/${mailbox}`)
+
+function read_email(email_id){
+
+    // Show the email reader and hide other views
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#read-view').style.display = 'block';
+
+    // Get the element and clear out anything in it from previous emails
+    const rdv = document.querySelector('#read-view');
+    rdv.innerHTML = '';
+
+    // Load the email reader
+    add_email_container(email_id, rdv);
+}
+
+function add_email_container(email_id, rdv){
+  fetch(`/emails/${email_id}`)
   .then(response => response.json())
-  .then(emails => {
-    console.log(emails);
-    emv.append(mailbox_as_div(emails));
+  .then(email =>{
+    console.log(email);
+    mark_as_read(email_id);
+    rdv.append(email_element(email))
+  })
+  .catch(error => {
+    console.log('Error: ', error);
+  });
+}
+
+function mark_as_read(email_id){
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true
+    })
   })
   .catch(error => {
     console.log('Error: ', error);
@@ -52,34 +86,108 @@ function load_emails(mailbox, emv){
 }
 
 
-function mailbox_as_div(emails){
+function email_element(email){
+  let ediv = document.createElement('div');
+  ediv.classList.add('container');
+
+  // Add rows for info at the top
+  ediv.append(add_read_row('From:', email.sender));
+  ediv.append(add_read_row('To:', email.recipients));
+  ediv.append(add_read_row('Sent:', email.timestamp));
+  ediv.append(add_read_row('Subject:', email.subject));
+
+  // Add hr
+  let my_hr = document.createElement('hr');
+  my_hr.id = 'reader-splitter';
+  ediv.append(my_hr);
+
+  // Add the email contents
+  const body = document.createElement('div');
+  body.id = 'reader-body';
+  body.innerHTML = email.body;
+  ediv.append(body);
+
+  return ediv;
+}
+
+
+function add_read_row(label, content){
+  // Create a row for this info
+  const rrow = new_row_div();
+
+  // Add a div for the label column
+  let tcol = document.createElement('div');
+  tcol.classList.add('col-2');
+  tcol.innerHTML = label;
+  rrow.append(tcol);
+
+  // Add a div to store the contents
+  tcol = document.createElement('div');
+  tcol.classList.add('col');
+  tcol.innerHTML = content;
+  rrow.append(tcol);
+
+  return rrow;
+}
+
+
+
+function new_row_div(){
+  const nrow = document.createElement('div');
+  nrow.classList.add('row');
+  return nrow;
+}
+
+
+
+
+// Loading all emails in a mailbox
+function add_mailbox_container(mailbox, emv){
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails);
+    emv.append(mailbox_element(emails));
+  })
+  .catch(error => {
+    console.log('Error: ', error);
+  });
+}
+
+// Create elements for a table showing a list of all emails in a particular mailbox
+function mailbox_element(emails){
   // Setup the container div
-  const div = document.createElement('div');
-  div.class = 'container mailbox-container';
+  const container = document.createElement('div');
+  container.classList.add('container', 'mailbox-container');
 
   // Start work on the HTML inside. Begin with the header.
-  let results = '<div class="row mailbox-header"><div class="col">Sender</div><div class="col">Subject</div><div class="col">Time</div></div>';
+  const hrow = document.createElement('div');
+  hrow.classList.add('row', 'mailbox-header');
+  hrow.innerHTML = '<div class="col">Sender</div><div class="col">Subject</div><div class="col">Time</div>';
+  container.append(hrow);
 
   // Add div rows for each email
   emails.forEach(function(email){
-
     // Start the row div, with different CSS for read vs. unread  
-    results += '<div class="';
+    const ediv = document.createElement('div');
+    ediv.classList.add('row');
+  
     if (email.read){
-      results += 'row read';
+      ediv.classList.add('read');
     }
     else{
-      results += 'row unread';
+      ediv.classList.add('unread');
     }
 
     // Add the info
-    results += `"><div class="col">${email.sender}</div><div class="col">${email.subject}</div><div class="col">${email.timestamp}</div>`;
-    results += '</div>';
+    ediv.innerHTML = `<div class="col">${email.sender}</div><div class="col">${email.subject}</div><div class="col">${email.timestamp}</div>`;
+    ediv.addEventListener('click', function(){
+      read_email(email.id)
+    });
+    container.append(ediv);
   });
 
-  // Add to div as innerHTML and return the div
-  div.innerHTML = results;
-  return div;
+  return container;
 }
 
 
